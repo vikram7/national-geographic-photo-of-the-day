8d9e2e32e6ea58155e1b5a0f20916e3735f112db
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-09-16 08:46:23 -0400

    added desktop picture reset shell script so that photo would update daily

--- ng_fetch.rb
+++ ng_fetch.rb
@@ -9,12 +9,14 @@ end
 link = open('http://photography.nationalgeographic.com/photography/photo-of-the-day/')
 prefix = "images.nationalgeographic.com/wpf/media-live"
 image = link.split("images.nationalgeographic.com/wpf/media-live")[1].split("/>")[0].split('"')[0]
-image_location = prefix + image
 
+image_location = prefix + image
 `curl #{image_location} > ng_daily_image.jpg`
 
 filepath = File.join(Dir.pwd, "ng_daily_image.jpg")
 
+`sh ./ng_reset.sh`
+sleep(1)
 `sh ./ng_set.sh #{filepath}`
 
 `rm ng_daily_image.jpg`
